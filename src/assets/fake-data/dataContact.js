398e347bc62a2d904dbbeb04d3a3f8cfd136d8f2
minhluc073 <nguyenminhluc0703@gmail.com>
2024-05-23 15:47:39 +0700

    update

--- src/assets/fake-data/dataContact.js
+++ src/assets/fake-data/dataContact.js
@@ -2,7 +2,7 @@ export const contactConfig = {
   YOUR_EMAIL: "themesflatc11@gmail.com",
   YOUR_FONE: "(84)922032190",
   description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra.",
   YOUR_SERVICE_ID: "service_nk03b9n",
   YOUR_TEMPLATE_ID: "template_9ckf6gs",
   YOUR_USER_ID: "eJfnZ4K8GglkEa7JB",
